38a5a443a60b67cc91b4d133e5133d46c919a80f
ahmed samir sakr <89707420+ahmedsamir010@users.noreply.github.com>
2022-01-22 23:06:15 +0200

    Add files via upload

---
+++ M. Distinct Numbers.cpp
@@ -0,0 +1,32 @@
+﻿//Link Problem==>
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<algorithm>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pi 3.141592653589793238
+#define ahmed  ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+int counter[1000001] = {};
+int main()
+{
+    ahmed
+        ll t, arr[10000], c = 0;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + t);
+    for (int i = 0; i < t; i++) {
+        if (arr[i] != arr[i + 1])c++;
+    }
+    cout << c << endl;
+  
+}
+//مهما الدنيا إتنيلت ضلمت لازم يبقى عندك زفت أمل و تعرف إن في نور في أخر أم النفق//
+
+        //اهو بنتنيل نستمر ف السعي رغم فقدان الزفت الشغف
